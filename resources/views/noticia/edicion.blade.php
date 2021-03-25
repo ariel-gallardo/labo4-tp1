df4b910f6c643f12d88f7c31dca33ef86c5e0853
Ariel Gallardo <ariel.gallardo@alumnos.frm.utn.edu.ar>
2021-03-24 22:24:14 -0300

    frontend# cambio plantilla tiny -> edicion

--- resources/views/noticia/edicion.blade.php
+++ resources/views/noticia/edicion.blade.php
@@ -1,10 +1,13 @@
-@extends('template.base')
+@extends('template.bootstrap')
+
 @section('script')
 <script src="https://cdn.tiny.cloud/1/470vl6oydu4y113xe2muphz55fln53vldzpewunxn0v858qz/tinymce/5/tinymce.min.js"
-  referrerpolicy="origin"></script>
+    referrerpolicy="origin"></script>
 <script src="{{asset('js/tiny.js')}}"></script>
 @endsection
+
 @section('content')
 <textarea id="editorHtml"></textarea>
 <button onclick="verHTML()">VER HTML</button>
 @endsection
+
